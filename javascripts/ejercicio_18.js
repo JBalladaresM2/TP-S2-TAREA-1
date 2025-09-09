f9e016bfb2ea416a89b07924d736eb5efcd7f297
st3benm <stevenmontalvan@gmail.com>
2025-09-09 12:07:30 -0500

    ola bb

--- javascripts/ejercicio_18.js
+++ javascripts/ejercicio_18.js
@@ -6,13 +6,13 @@ function ejecutar_18(){
     const valor=30;
     destino=prompt("Ingrese el destino del paquete (Nacional o internacional)")     //EL VALOR YA ESTA PUESTO Y SOLAMENTE ABRIA QUE INGRESAR EL PESO 
     let n=parseInt(prompt("Ingrese las veces que va a realizar un pedido"))
-    let c=1                                                                         //CREAMOS UN WHILE Y HACEMOS EL BUCLE 
+    let c=0                                                                       //CREAMOS UN WHILE Y HACEMOS EL BUCLE 
     while(c<n){
         peso=parseInt(prompt("Ingrese el peso del paquete en kg"))
-        if (peso<=5){
+        if (peso<5){
             total=valor*peso                                                                              //CALCULAMOS Y PRESENTAMOS
             console.log(`Su paquete se dirige a ${destino} con un peso de ${peso}kg ,  el precio por kg es de ${valor}$ y el costo es de ${total} dolares `)
-        } else if (peso>6){
+        } else if (peso>=5){
             console.log(`Su paquete excede el peso permitido`)
         }
         c++;
